00fe8dca46e32d47707924909f3af91ad4aa83ec
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2025-05-12 15:22:36 -0700

    implement a Singly Linked List

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,70 +1,64 @@
-import java.io.*; 
-  
+import java.io.*;
+// Time Complexity: push, pop, peek, isEmpty: O(1)
+// Space Complexity: O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
-  
-    Node head; // head of list 
-  
-    // Linked list Node. 
-    // This inner class is made static 
-    // so that main() can access it 
-    static class Node { 
-  
-        int data; 
-        Node next; 
-  
-        // Constructor 
-        Node(int d) 
-        { 
-            //Write your code here 
-        } 
-    } 
-  
-    // Method to insert a new node 
-    public static LinkedList insert(LinkedList list, int data) 
-    { 
-        // Create a new node with given data 
-   
-        // If the Linked List is empty, 
-        // then make the new node as head 
-        
-            // Else traverse till the last node 
-            // and insert the new_node there 
+public class Exercise_3 {
 
-            // Insert the new_node at last node 
-        // Return the list by head 
-        
-    } 
-  
-    // Method to print the LinkedList. 
-    public static void printList(LinkedList list) 
-    {  
-        // Traverse through the LinkedList 
-   
-            // Print the data at current node 
-       
-            // Go to next node 
-    } 
-   
-    // Driver code 
-    public static void main(String[] args) 
-    { 
-        /* Start with the empty list. */
-        LinkedList list = new LinkedList(); 
-  
-        // 
-        // ******INSERTION****** 
-        // 
-  
-        // Insert the values 
-        list = insert(list, 1); 
-        list = insert(list, 2); 
-        list = insert(list, 3); 
-        list = insert(list, 4); 
-        list = insert(list, 5); 
-  
-        // Print the LinkedList 
-        printList(list); 
-    } 
+    Node head;
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static Exercise_3 insert(Exercise_3 list, int data) {
+
+        Node newNode = new Node(data);
+
+        if (list.head == null) {
+            list.head = newNode;
+        } else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+            last.next = newNode;
+        }
+
+        return list;
+    }
+
+    public static void printList(Exercise_3 list) {
+        Node currNode = list.head;
+
+        System.out.print("LinkedList: ");
+
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+        System.out.println();
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        Exercise_3 list = new Exercise_3();
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        printList(list);
+    }
 }
